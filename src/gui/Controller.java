c299630b726d8fef2ae0927deae9b8a9f5ee07ca
mirasvarc <mirasvarc1@gmail.com>
2019-05-11 16:30:36 +0200

    edit gui, add load from file

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -2,24 +2,54 @@ package gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.scene.control.TextArea;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.ArrayList;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+
+
+import javafx.stage.Stage;
+
 
 import Chess_common.*;
 
 
-
 public class Controller {
-    @FXML private GridPane chessBoard;
-    private Board board = new Board();
-
+    @FXML private GridPane chessBoardView;
+    @FXML private Pane main;
+    private Board chessBoard = new Board();
+    private Pane board[][] = new Pane[8][8];
 
     Tab tab = new Tab();
-
+    @FXML ListView<String> listView;
     @FXML TextArea text; // TODO TextField?
 
-    @FXML protected void onMouseClick(){
+    @FXML protected void onMouseClick(MouseEvent event){
+        for(Node node : chessBoardView.getChildren()){
+            if (node instanceof Pane){
+                Pane pane = (Pane) node;
+                board[Character.getNumericValue(pane.getId().charAt(4))][Character.getNumericValue(pane.getId().charAt(6))] = pane;
+            }
+        }
+
+        double width = chessBoardView.getWidth();
+        double height = chessBoardView.getHeight();
+
+        int col = (int)(event.getX()/width*8);
+        int row = (int)(event.getY()/height*8);
+        //System.out.println(row + " : " + col);
+
 
     }
 
@@ -48,11 +78,34 @@ public class Controller {
     }
 
     @FXML protected void load(ActionEvent event){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        File file = fileChooser.showOpenDialog(main.getScene().getWindow());
+        System.out.println(file);
+        ArrayList<String> lines = new ArrayList<String>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+                listView.getItems().add(line);
+            }
+
+            br.close();
+        }
+        catch(Exception e){
+
+        }
+
+        System.out.println(lines);
+
+
         String all_moves = "1. Jc3 a5\n" +
                 "2. Jd5 J8f6\n" +
                 "3. Jxf6+ c7c6\n" +
                 "4. Dxa5 f7f5\n";
-        text.appendText(all_moves);
+
     }
 
     @FXML protected void save(ActionEvent event){
--- src/gui/Main.java
+++ src/gui/Main.java
@@ -2,32 +2,62 @@ package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
+import javafx.scene.Parent;
 import Chess_common.*;
 
-import java.awt.*;
 
 
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        FXMLLoader.setDefaultClassLoader(Thread.currentThread().getContextClassLoader());
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
-
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        //loader.setDefaultClassLoader(Thread.currentThread().getContextClassLoader());
+        loader.setLocation(Main.class.getResource("sample.fxml"));
+        Parent root = loader.load();
 
         primaryStage.setTitle("Chess");
-        primaryStage.setScene(new Scene(root, 900, 700));
+        primaryStage.setScene(new Scene(root, 800, 700));
         primaryStage.show();
+
+        FXMLLoader loader1 = new FXMLLoader();
+        loader1.setLocation(Main.class.getResource("chessBoard.fxml"));
+        GridPane chessBoard = loader1.load();
+        chessBoard.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+
+
+        for (Node node : chessBoard.getChildren()) {
+            Integer x = GridPane.getRowIndex(node);
+            Integer y = GridPane.getColumnIndex(node);
+            int row = x == null ? 0 : x;
+            int col = y == null ? 0 : y;
+            if (node instanceof Pane) {
+                node.setId("pane" + row + "_" + col);
+            }
+            row = 8 - row - 1;
+            if ((row + col) % 2 == 1) {
+                node.setStyle("-fx-background-color: rgb(255,255,255);");
+            } else {
+                node.setStyle("-fx-background-color: rgb(128,128,128);");
+            }
+            if (row == 1) {
+                ImageView img = new ImageView(new Image(getClass().getResourceAsStream("../chess_figures_img/Pawn_B.png")));
+                if (node instanceof Pane) {
+                    ((Pane) node).getChildren().add(img);
+                }
+            }
+        }
+
     }
-
-
 
 
     public static void main(String[] args) {
