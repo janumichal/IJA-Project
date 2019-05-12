ad029bb43c8b45f74b826a3da1162b2d8372913b
Yumichy <43389119+Yumichy@users.noreply.github.com>
2019-05-12 15:37:24 +0200

    moves

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -1,8 +1,12 @@
 package gui;
 
+import Chess_pieces.*;
+import enums.color_piece;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -12,7 +16,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.FileChooser;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
-
 
 import java.util.Set;
 import java.io.*;
@@ -25,7 +28,6 @@ import javafx.collections.FXCollections;
 
 import javafx.stage.Stage;
 
-import Chess_pieces.Piece;
 import Chess_common.*;
 
 
@@ -37,20 +39,20 @@ public class Controller {
     Boolean click = false;
     private Board chessBoard = new Board();
     private Pane board[][] = new Pane[8][8];
-    Field from = new Field(0,0);
-    Field to = new Field(0,0);
+    Field from;
+    Field to;
 
 
     Tab tab = new Tab();
     @FXML ListView<String> listView;
 
     @FXML protected void onMouseClick(MouseEvent event){
-        for(Node node : chessBoardView.getChildren()){
-            if (node instanceof Pane){
-                Pane pane = (Pane) node;
-                board[Character.getNumericValue(pane.getId().charAt(4))][Character.getNumericValue(pane.getId().charAt(6))] = pane;
-            }
-        }
+//        for(Node node : chessBoardView.getChildren()){
+//            if (node instanceof Pane){
+//                Pane pane = (Pane) node;
+//                board[Character.getNumericValue(pane.getId().charAt(4))][Character.getNumericValue(pane.getId().charAt(6))] = pane;
+//            }
+//        }
 
         double width = chessBoardView.getWidth();
         double height = chessBoardView.getHeight();
@@ -59,13 +61,15 @@ public class Controller {
             int row = (int) (event.getY() / height * 8);
             click = true;
             System.out.println("From " + row + " : " + col);
-
-            currentPiece = tab.game.board.getField(row, col).getPiece();
+//            tab.game.board.showPiecesText();
+            from = tab.game.board.getField(col,row);
+            currentPiece = from.getPiece();
+            System.out.println(currentPiece instanceof Pawn ? "ano" : "ne");
             if (currentPiece == null) return;
 
-            from.setRow(row);
-            from.setCol(col);
-            System.out.println(from.getPiece());
+//            from.setRow(row);
+//            from.setCol(col);
+
         }
         else if (click){
             int col2 = (int) (event.getX() / width * 8);
@@ -73,12 +77,10 @@ public class Controller {
             System.out.println("To " + row2 + " : " + col2);
             click = false;
 
-            to.setRow(row2);
-            to.setCol(col2);
+            to = tab.game.board.getField(col2,row2);
+            tab.move(from, to);
+            drawBoard(chessBoardView,tab);
         }
-
-
-       tab.move(from, to);
 
 
     }
@@ -137,4 +139,74 @@ public class Controller {
     @FXML protected void gameType(ActionEvent event){
         // TODO
     }
+
+        public void drawBoard(Pane pane, Tab tab){
+            for (Node node : pane.getChildren()) {
+                int img_size = 50;
+                Integer y = GridPane.getColumnIndex(node);
+                Integer x = GridPane.getRowIndex(node);
+                int row = y == null ? 0 : y;
+                int col = x == null ? 0 : x;
+                if (node instanceof Pane) {
+                    node.setId("pane" + row + "_" + col);
+                }
+
+                if ((row + col) % 2 == 0) {
+                    node.setStyle("-fx-background-color: rgb(255,255,255);");
+                } else {
+                    node.setStyle("-fx-background-color: rgb(128,128,128);");
+                }
+
+
+
+                ImageView img;
+
+                Piece piece = tab.game.board.getField(row, col).getPiece();
+                if(piece instanceof Rook){
+                    if (piece.getColor() == color_piece.WHITE){
+                        img = new ImageView(new Image("chess_figures_img/Rook_w.png"));
+                    }else{
+                        img = new ImageView(new Image("chess_figures_img/Rook_b.png"));
+                    }
+                }else if(piece instanceof Knight){
+                    if (piece.getColor() == color_piece.WHITE){
+                        img = new ImageView(new Image("chess_figures_img/Knight_w.png"));
+                    }else{
+                        img = new ImageView(new Image("chess_figures_img/Knight_b.png"));
+                    }
+                }else if(piece instanceof Bishop){
+                    if (piece.getColor() == color_piece.WHITE){
+                        img = new ImageView(new Image("chess_figures_img/Bishop_w.png"));
+                    }else{
+                        img = new ImageView(new Image("chess_figures_img/Bishop_b.png"));
+                    }
+                }else if(piece instanceof King){
+                    if (piece.getColor() == color_piece.WHITE){
+                        img = new ImageView(new Image("chess_figures_img/King_w.png"));
+                    }else{
+                        img = new ImageView(new Image("chess_figures_img/King_b.png"));
+                    }
+                }else if(piece instanceof Queen){
+                    if (piece.getColor() == color_piece.WHITE){
+                        img = new ImageView(new Image("chess_figures_img/Queen_w.png"));
+                    }else{
+                        img = new ImageView(new Image("chess_figures_img/Queen_b.png"));
+                    }
+                }else if(piece instanceof Pawn){
+                    if (piece.getColor() == color_piece.WHITE){
+                        img = new ImageView(new Image("chess_figures_img/Pawn_w.png"));
+                    }else{
+                        img = new ImageView(new Image("chess_figures_img/Pawn_B.png"));
+                    }
+                }else{
+                    img = new ImageView();
+                }
+
+                if (node instanceof Pane){
+                    img.setFitHeight(img_size);
+                    img.setFitWidth(img_size);
+                    ((Pane)node).getChildren().add(img);
+                }
+            }
+        }
 }
